76e8cb4b019825378e15a691c2a1b32a96d84fa1
STUTuna <s15113114@stu.edu.tw>
2021-02-18 23:23:15 +0800

    Update app.py

--- app.py
+++ app.py
@@ -33,8 +33,8 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    get_message = event.message.text
-    get_message = get_message + '字尾'
+    get_message = event.message.text + '字尾'
+    
     # Send To Line
     reply = TextSendMessage(text=f"{get_message}")
     line_bot_api.reply_message(event.reply_token, reply)
